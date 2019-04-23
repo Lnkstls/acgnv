bfbe0671c1990cecac62b6a74de99a96e551c9a9
lnkstls <lnkstls@gmail.com>
2019-04-23 19:05:28 +0800

    X_js/scripts-a.js_2

--- js/scripts-a.js
+++ js/scripts-a.js
@@ -1,3 +1,3 @@
 $(".github-logo").click(function(){
-		window.location.href="https://github.com/lnkstls/lnkstls.github.io/issues/new";
+		window.location.href="https://github.com/lnkstls/acgnv/issues/new";
 });
